a900a4433e0edcd9431b531038810846f0c1f3e6
anthsnow <ivanovai@inteltech.ru>
2021-12-13 13:58:51 +0300

    Update CSSMinifier

--- src/Core/AssetPipeline/CSSMinifier.php
+++ src/Core/AssetPipeline/CSSMinifier.php
@@ -7,44 +7,34 @@ class CSSMinifier
     static public function minify(string $path): string {
         $tmpCSS = "";
         if ($file = fopen($path, "r")) {
-            $currentLine = 0;
-            $isMultiLine = false;
+            $isComment = false;
             while (!feof($file)) {
-                $currentLine += 1;
-                $line = fgets($file);
-                $tmpLine = trim($line);
+                $tmpLine = trim(fgets($file));
                 if (empty($tmpLine))
                     continue; // ignore empty line
 
                 // --- check multi line comments ---
-                $mlComment = self::mlComment($tmpLine);
-                $tmpLineStart = "";
-                $tmpLineEnd = "";
-                if ($mlComment['start'] != -1) {
-                    $isMultiLine = true;
-                    $tmpLineStart = substr($tmpLine, 0, $mlComment['start']);
-                }
-                if ($mlComment['end'] != -1) {
-                    $isMultiLine = false;
-                    $tmpLineEnd = substr($tmpLine, $mlComment['end'], strlen($tmpLine));
-                }
-                if ($mlComment['start'] != -1
-                    || $mlComment['end'] != -1
-                    || $isMultiLine === true)
-                    $tmpLine = $tmpLineStart.$tmpLineEnd;
+                $tmpLine = self::multiLineComment($tmpLine, $isComment);
 
-                if (!$isMultiLine) {
-                    // --- check one line comment ---
-                    $olComment = self::comment($tmpLine);
-                    $tmpLineStart = "";
-                    $tmpLineEnd = "";
-                    if ($olComment['start'] != -1)
-                        $tmpLineStart = substr($tmpLine, 0, $olComment['start']);
-                    if ($olComment['end'] != -1)
-                        $tmpLineEnd = substr($tmpLine, $olComment['end'], strlen($tmpLine));
-                    if ($olComment['start'] != -1 || $olComment['end'] != -1)
-                        $tmpLine = $tmpLineStart . $tmpLineEnd;
-                }
+                // --- check one line comment ---
+                if (!$isComment)
+                    $tmpLine = self::oneLineComment($tmpLine);
+
+                // --- change white-space(s) ---
+                if (!empty($tmpLine) && preg_match('/(\s{2,})/', $tmpLine, $matches))
+                    $tmpLine = str_replace($matches[0], " ", $tmpLine);
+
+                // --- change open selector ---
+                if (!empty($tmpLine) && preg_match('/(\s*\{\s*)/', $tmpLine, $matches))
+                    $tmpLine = str_replace($matches[0], "{", $tmpLine);
+
+                // --- change close selector ---
+                if (!empty($tmpLine) && preg_match('/(\s*\}\s*)/', $tmpLine, $matches))
+                    $tmpLine = str_replace($matches[0], "}", $tmpLine);
+
+                // --- change key-value splitter ---
+                if (!empty($tmpLine) && preg_match('/(\s*\:\s*)/', $tmpLine, $matches))
+                    $tmpLine = str_replace($matches[0], ":", $tmpLine);
 
                 // --- change '0(cm|em|ex|in|mm|pc|pt|px|vh|vw|%)' to '0' ---
                 if (!empty($tmpLine) && preg_match('/(?<=[\s:])(0)(cm|em|ex|in|mm|pc|pt|px|vh|vw|%)/', $tmpLine, $matches))
@@ -54,18 +44,19 @@ class CSSMinifier
                 if (!empty($tmpLine) && preg_match('/(?<=[\s:])(0\s+0|0\s+0\s+0\s+0)(?=[;\}]|\!important)/', $tmpLine, $matches))
                     $tmpLine = str_replace($matches[0], "0", $tmpLine);
 
-                // change '0.6' to '.6'
+                // --- change '0.6' to '.6' ---
                 if (!empty($tmpLine) && preg_match('/(?<=[\s:,\-])0+\.(\d+)/', $tmpLine, $matches))
                     $tmpLine = str_replace($matches[0], ".".$matches[1], $tmpLine);
 
-                // change HEX color (example: '#ffffff' to '#fff')
+                // --- change HEX color (example: '#ffffff' to '#fff') ---
                 if (!empty($tmpLine) && preg_match('/(?<=[\s:,\-]\#)([a-f0-6]+)\1([a-f0-6]+)\2([a-f0-6]+)\3/', $tmpLine, $matches))
                     $tmpLine = str_replace($matches[0], $matches[1].$matches[2].$matches[3], $tmpLine);
 
-                // change '(border|outline):none' to '(border|outline):0'
+                // --- change '(border|outline):none' to '(border|outline):0' ---
                 if (!empty($tmpLine) && preg_match('/(?<=[\{;\s])*(border|outline)\s*\:\s*none(?=[;\}\!])/', $tmpLine, $matches))
                     $tmpLine = str_replace($matches[0], $matches[1].":0", $tmpLine);
 
+                // --- build css ---
                 if (!empty($tmpLine)) {
                     $tmpCSS .= $tmpLine;
 
@@ -76,64 +67,98 @@ class CSSMinifier
             }
             fclose($file);
         }
-        $tmpCSS = str_replace(" {", "{", $tmpCSS);
-        $tmpCSS = str_replace("{ ", "{", $tmpCSS);
-        $tmpCSS = str_replace("} ", "}", $tmpCSS);
-        $tmpCSS = str_replace(" }", "}", $tmpCSS);
-        $tmpCSS = str_replace(": ", ":", $tmpCSS);
         return trim($tmpCSS);
     }
 
-    static private function comment(string $line) {
-        $mlStart = -1;
-        $mlEnd = -1;
+    // --- private functions ---
+
+    static private function oneLineComment(string $line): string {
+        if (empty($line))
+            return $line;
+
         $pos = strpos($line, "//");
-        if ($pos !== false) {
+        if ($pos === false)
+            return $line;
+        $quote = false;
+        $dblQuote = false;
+        $prevChar = null;
+        for ($i = 0; $i < $pos; $i++) {
+            $char = $line[$i];
+            if ($i > 0)
+                $prevChar = $line[$i - 1];
+            if (strcmp($char, "'") === 0 && !$dblQuote
+                && !is_null($prevChar) && strcmp($prevChar, "\\") !== 0)
+                $quote = !$quote;
+            else if (strcmp($char, "\"") === 0 && !$quote
+                && !is_null($prevChar) && strcmp($prevChar, "\\") !== 0)
+                $dblQuote = !$dblQuote;
+        }
+        if ($quote === false && $dblQuote === false)
+            $line = substr($line, 0, $pos);
+
+        return trim($line);
+    }
+
+    static private function multiLineComment(string $line, bool &$isComment): string {
+        if (empty($line))
+            return $line;
+
+        $checkArray = [ "/*", "*/" ];
+        if ($isComment)
+            array_shift($checkArray);
+
+        $mlStart = -1;
+        $mlEnd =  -1;
+        foreach ($checkArray as $chk) {
+            $pos = strpos($line, $chk);
+            if ($pos === false)
+                continue;
             $quote = false;
             $dblQuote = false;
+            $prevChar = null;
             for ($i = 0; $i < $pos; $i++) {
-                if (strcmp($line[$i], "'") === 0)
+                $char = $line[$i];
+                if ($i > 0)
+                    $prevChar = $line[$i - 1];
+                if (strcmp($char, "'") === 0 && !$dblQuote
+                    && !is_null($prevChar) && strcmp($prevChar, "\\") !== 0)
                     $quote = !$quote;
-                if (strcmp($line[$i], "\"") === 0)
+                else if (strcmp($char, "\"") === 0 && !$quote
+                    && !is_null($prevChar) && strcmp($prevChar, "\\") !== 0)
                     $dblQuote = !$dblQuote;
             }
             if ($quote === false && $dblQuote === false) {
-                $mlStart = $pos;
-                $mlEnd = strlen($line);
+                switch ($chk) {
+                    case "/*":
+                        $mlStart = $pos;
+                        break;
+                    case "*/":
+                        $mlEnd = $pos + 2;
+                        break;
+                    default:
+                        break;
+                }
             }
         }
-        return [ 'start' => $mlStart, 'end' => $mlEnd ];
-    }
+        $tmpLineStart = "";
+        $tmpLineEnd = "";
+        if ($mlStart !== -1) {
+            $isComment = true;
+            $tmpLineStart = substr($line, 0, $mlStart);
+        }
+        if ($mlEnd !== -1) {
+            $isComment = false;
+            $tmpLineEnd = substr($line, $mlEnd, strlen($line));
+        }
+        if ($mlStart !== -1 && $mlEnd !== -1)
+            $line = self::multiLineComment($tmpLineStart . $tmpLineEnd, $isComment);
+        else if ($mlEnd !== -1)
+            $line = self::multiLineComment($tmpLineEnd, $isComment);
+        else if ($mlStart !== -1)
+            $line = $tmpLineStart;
+        else if ($isComment)
+            $line = "";
 
-    static private function mlComment(string $line) {
-        $mlStart = -1;
-        $mlEnd =  -1;
-        $pos = strpos($line, "/*");
-        if ($pos !== false) {
-            $quote = false;
-            $dblQuote = false;
-            for ($i = 0; $i < $pos; $i++) {
-                if (strcmp($line[$i], "'") === 0)
-                    $quote = !$quote;
-                if (strcmp($line[$i], "\"") === 0)
-                    $dblQuote = !$dblQuote;
-            }
-            if ($quote === false && $dblQuote === false)
-                $mlStart = $pos;
-        }
-        $pos = strpos($line, "*/");
-        if ($pos !== false) {
-            $quote = false;
-            $dblQuote = false;
-            for ($i = 0; $i < $pos; $i++) {
-                if (strcmp($line[$i], "'") === 0)
-                    $quote = !$quote;
-                if (strcmp($line[$i], "\"") === 0)
-                    $dblQuote = !$dblQuote;
-            }
-            if ($quote === false && $dblQuote === false)
-                $mlEnd = $pos + 2;
-        }
-        return [ 'start' => $mlStart, 'end' => $mlEnd ];
+        return trim($line);
     }
 }
